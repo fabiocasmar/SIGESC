cc6a1bfd2598c7940e8c44921d3e0fb6a3dd3582
Gabo <gabohd@gmail.com>
2015-01-24 12:30:08 -0430

    Se agregan los catalogos predefinidos de django que utilizo el otro grupo, durante el dia cambiare los templates a español y a ponerle las funcionalidades como el buscador, si alguien tiene alguna pregunta o estan trabajando en django me puede agregar a skype gabo120878 y decirme cualquier cosa por ahi!

--- entidades/admin.py
+++ entidades/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from entidades import models    
 
 # Register your models here.
+@admin.register(models.Cursa)
+class adminCursa(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Tutor)
+class adminTutor(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Estudiante)
+class adminEstudiante(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Proyecto)
+class adminProyecto(admin.ModelAdmin):
+    pass
