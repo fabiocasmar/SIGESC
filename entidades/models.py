7b52426304773fba1ab526f02099cdbd6fdc5241
Daniel Pelayo <daniel.pelayo23@gmail.com>
2015-01-26 11:12:50 -0430

    el modelo valida carnet de estudiantes

--- entidades/models.py
+++ entidades/models.py
@@ -10,6 +10,17 @@ Created on 22/1/2015
 '''
 
 from django.db import models
+import re
+from django.core.exceptions import ValidationError
+
+def validarCarnet(carnet):
+    
+    if not re.match(r'[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9]',carnet):
+        raise ValidationError(u'%s Carnet no valido. Debe ser de la forma: XX-XXXXX' % carnet)
+    else:
+        pass
+        
+    
 
 class Estudiante(models.Model):
     tipo_sexo=(("M","Masculino"), ("F","Femenino"),)
@@ -17,16 +28,16 @@ class Estudiante(models.Model):
                     ('0400','Ing. Produccion'),('0500','Ing. Materiales'),('0600','Ing. Quimica'),('0700','Ing. Geofisica'))
     tipo_sede = (('S','Sartenejas'),('L','Litoral'))
         
-    USBID = models.CharField(max_length = 8,primary_key = True)
-    nombre = models.CharField(max_length = 30)
-    apellido = models.CharField(max_length = 30)
-    cedula = models.CharField(max_length = 8)
-    carrera = models.CharField(max_length = 4,choices = tipo_carrera)
-    sede = models.CharField(max_length = 1, choices = tipo_sede)
-    email = models.CharField(max_length = 30)
-    sexo = models.CharField(max_length = 1, choices = tipo_sexo)
-    telefono = models.CharField(max_length = 12)
-    direccion = models.CharField(max_length = 50)
+    USBID = models.CharField(max_length = 8, primary_key = True, validators = [validarCarnet])
+    nombre = models.CharField(max_length = 30, blank = False)
+    apellido = models.CharField(max_length = 30, blank = False)
+    cedula = models.CharField(max_length = 8, blank = False)
+    carrera = models.CharField(max_length = 4, choices = tipo_carrera, blank = False)
+    sede = models.CharField(max_length = 1, choices = tipo_sede, blank = False)
+    email = models.EmailField(max_length = 75, blank = False)
+    sexo = models.CharField(max_length = 1, choices = tipo_sexo, blank = False)
+    telefono = models.CharField(max_length = 12, blank = False)
+    direccion = models.CharField(max_length = 50, blank = False)
     
     def __str__(self):              # __unicode__ on Python 2
         return self.nombre + " " + self.apellido + " carnet: " + self.USBID
@@ -36,28 +47,29 @@ class Tutor(models.Model):
     tipo_sexo=(("M","Masculino"),("F","Femenino"))
     tipo_sede = (('S','Sartenejas'),('L','Litoral'))
     
-    USBID = models.CharField(max_length = 20,primary_key = True)
-    cedula = models.CharField(max_length = 8)    
-    nombre = models.CharField(max_length = 30)
-    apellido = models.CharField(max_length = 30)
-    sede = models.CharField(max_length = 1,choices = tipo_sede)
-    email = models.CharField(max_length = 30)
-    sexo = models.CharField(max_length = 1, choices = tipo_sexo)
-    telefono = models.CharField(max_length = 12)
-    direccion = models.CharField(max_length = 50)
+    USBID = models.EmailField(max_length = 75, primary_key = True)
+    cedula = models.CharField(max_length = 8, blank = False)    
+    nombre = models.CharField(max_length = 30, blank = False)
+    apellido = models.CharField(max_length = 30, blank = False)
+    sede = models.CharField(max_length = 1, choices = tipo_sede, blank = False)
+    email = models.EmailField(max_length = 30, blank = False)
+    sexo = models.CharField(max_length = 1, choices = tipo_sexo, blank = False)
+    telefono = models.CharField(max_length = 12, blank = False)
+    direccion = models.CharField(max_length = 50, blank = False)
     
     def __str__(self):              # __unicode__ on Python 2
         return self.nombre + " " + self.apellido
 
     
 class Proyecto(models.Model):
-    #tipo_sexo=(("A","Activo"),("",""))
+    tipo_estado_proyecto=(("A","Abierto"),("C","Cerrado"),('P','En Proceso'))
+    
     cod_proyecto = models.CharField(max_length = 7,primary_key = True)
-    nombre = models.CharField(max_length = 30)
-    descripcion = models.CharField(max_length = 100)
-    area = models.CharField(max_length = 40)
-    estado = models.CharField(max_length = 10)
-    tutor = models.ForeignKey(Tutor)
+    nombre = models.CharField(max_length = 30, blank = False)
+    descripcion = models.CharField(max_length = 100, blank = False)
+    area = models.CharField(max_length = 40, blank = False)
+    estado = models.CharField(max_length = 10, blank = False, choices = tipo_estado_proyecto)
+    tutor = models.ForeignKey(Tutor, blank = False)
     
     def __str__(self):              # __unicode__ on Python 2
         return self.cod_proyecto + " " +self.nombre
