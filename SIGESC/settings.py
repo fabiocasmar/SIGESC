93ab1328fc08a5bec2b6875049e844d2a141a17c
Fabio Castro <fabiocastro100@gmail.com>
2015-01-26 23:14:38 -0430

    Modificadas las rutas a rutas relativas

--- SIGESC/settings.py
+++ SIGESC/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-...
+
 
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
