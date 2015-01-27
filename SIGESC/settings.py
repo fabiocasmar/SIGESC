c2bd96b7e49f427f407541f1221e42eb102b1128
Fabio Castro <fabiocastro100@gmail.com>
2015-01-26 23:53:46 -0430

    Modificado el settings.py para que funciones en el servidor

--- SIGESC/settings.py
+++ SIGESC/settings.py
@@ -103,3 +103,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ("/var/www/SIGESC/static",)
+STATIC_ROOT = "/var/www/SIGESC/"
